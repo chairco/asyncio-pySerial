80e59704803f6972511157e432910ad53325e81f
chairco <chairco@gmail.com>
2019-03-18 10:49:44 +0800

    Update stream

--- stream.py
+++ stream.py
@@ -5,24 +5,26 @@ import random
 
 
 # serial setting
-url = '/dev/cu.usbmodem1411'
+url = '/dev/cu.usbmodem1421'
+url2 = '/dev/cu.usbserial-AL016RPE'
 port = 9600
 
-async def produce(queue, **kwargs):
-    """get serial data use recv() define format with non-blocking 
+
+async def produce(queue, url, **kwargs):
+    """get serial data use recv() define format with non-blocking
     """
     reader, writer = await serial_asyncio.open_serial_connection(url=url, **kwargs)
     buffers = recv(reader)
     async for buf in buffers:
         # TODO: can handle data format here
-        print(f"produce id: {id(buf)}")
+        print(f"produce id: {id(buf)}, device:{buf.split(',')[2:4]}")
         await asyncio.sleep(random.random())
         await queue.put(buf)
 
 
 async def recv(r):
     """
-    Handle stream data with different StreamReader: 
+    Handle stream data with different StreamReader:
     'read', 'readexactly', 'readuntil', or 'readline'
     """
     while True:
@@ -47,7 +49,8 @@ async def consume(queue):
 
 loop = asyncio.get_event_loop()
 queue = asyncio.Queue(loop=loop)
-producer_coro = produce(queue, baudrate=port)
+producer_coro = produce(queue, url=url, baudrate=port)
+producer_coro2 = produce(queue, url=url2, baudrate=port)
 consumer_coro = consume(queue)
-loop.run_until_complete(asyncio.gather(producer_coro, consumer_coro))
+loop.run_until_complete(asyncio.gather(producer_coro, producer_coro2, consumer_coro))
 loop.close()
